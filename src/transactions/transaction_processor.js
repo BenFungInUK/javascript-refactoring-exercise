7f818175b46c60814072b654f872a4f194e171ad
BenFung <future28uk2021@gmail.com>
2022-03-01 17:35:19 +0000

    Refactor sorting function in single line code

--- src/transactions/transaction_processor.js
+++ src/transactions/transaction_processor.js
@@ -3,25 +3,21 @@ function processTransactions(transActions) {
     throw new Error("Undefined collection of transactions");
   }
 
-  let txCount = {};
+  const txCount = {};
 
   transActions.forEach((item) =>
     txCount[item] ? (txCount[item] += 1) : (txCount[item] = 1)
   );
 
-  return sortByAmountThenName(txCount);
-}
-
-function sortByAmountThenName(txCount) {
-  let sortedKeys = Object.keys(txCount).sort((itemOne, itemTwo) => {
-    return (
-      txCount[itemTwo] - txCount[itemOne] ||
-      itemOne > itemTwo ||
-      -(itemOne < itemTwo)
-    );
-  });
-
-  return sortedKeys.map((item) => `${item} ${txCount[item]}`);
+  return Object.keys(txCount)
+    .sort((itemOne, itemTwo) => {
+      return (
+        txCount[itemTwo] - txCount[itemOne] ||
+        itemOne > itemTwo ||
+        -(itemOne < itemTwo)
+      );
+    })
+    .map((item) => `${item} ${txCount[item]}`);
 }
 
 module.exports = processTransactions;
